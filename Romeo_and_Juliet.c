b724d0dca56ef012d8d27040cf5f03e4014bee16
23MHCQ <147491408+23MHCQ@users.noreply.github.com>
2023-12-05 13:39:58 +0530

    # include<stdio.h> int main() {     int x,y,z,n,m;     scanf("%d%d%d",&x,&y,&z);     n=x*5+y*10;     if(n>2)     {         m=n/z;         printf("%d",m);
        }
        else
        {
            printf("0");
        }
    }

---
+++ Romeo_and_Juliet.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+int main()
+{
+    int x,y,z,n,m;
+    scanf("%d%d%d",&x,&y,&z);
+    n=x*5+y*10;
+    if(n>2)
+    {
+        m=n/z;
+        printf("%d",m);
+        
+    }
+    else
+    {
+        printf("0");
+    }
+}
