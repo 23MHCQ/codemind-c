49b6624789a241704c0347cf346fc4866de720bb
23MHCQ <147491408+23MHCQ@users.noreply.github.com>
2023-12-24 16:08:43 +0530

    #include<stdio.h> int main() {     int x,b,a;     scanf("%d%d%d",&x,&a,&b);     if(a*1+b*2<x)     {         printf("Not Qualify");     }     else     {         printf("Qualify");     } }

---
+++ Contest:.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x,b,a;
+    scanf("%d%d%d",&x,&a,&b);
+    if(a*1+b*2<x)
+    {
+        printf("Not Qualify");
+    }
+    else
+    {
+        printf("Qualify");
+    }
+}
