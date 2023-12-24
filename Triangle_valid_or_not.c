78b275a7689a2aad17a46f68b799f22a8f67cb76
23MHCQ <147491408+23MHCQ@users.noreply.github.com>
2023-12-24 14:35:01 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a+b>c)     {         if(b+c>a)         {             if(a+c>b)             {                 printf("Valid triangle");             }             else             {                 printf("Invalid triangle");             }         }         else         {             printf("Invalid triangle");         }
        }
        else
        {
            printf("Invalid triangle");
        }
    }

---
+++ Triangle_valid_or_not.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a+b>c)
+    {
+        if(b+c>a)
+        {
+            if(a+c>b)
+            {
+                printf("Valid triangle");
+            }
+            else
+            {
+                printf("Invalid triangle");
+            }
+        }
+        else
+        {
+            printf("Invalid triangle");
+        }
+        
+    }
+    else
+    {
+        printf("Invalid triangle");
+    }
+}
